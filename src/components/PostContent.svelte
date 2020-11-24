a343bf480d32aa0095088f021a1cba7fe038085d
Thomas Hurtig <thurtig.email@gmail.com>
2020-11-24 15:16:04 +0100

    put prism parsing into action

--- src/components/PostContent.svelte
+++ src/components/PostContent.svelte
@@ -1,10 +1,11 @@
 <script>
-  import Prism from 'prismjs';
-  import marked from 'marked';
-  import PostDate from './PostDate.svelte';
-  import PostTitle from './PostTitle.svelte';
-  import GithubLink from './GithubLink.svelte';
-  import Tags from './Tags.svelte';
+  import { prism } from "../actions/prism";
+  import { onMount } from "svelte";
+  import marked from "marked";
+  import PostDate from "./PostDate.svelte";
+  import PostTitle from "./PostTitle.svelte";
+  import GithubLink from "./GithubLink.svelte";
+  import Tags from "./Tags.svelte";
 
   export let title;
   export let created_at;
@@ -12,52 +13,31 @@
   export let github;
   export let content;
 
-  let content_parsed = '';
+  let content_parsed;
 
-  const options = {
-    highlight: function (code, lang, callback) {
-      if(!callback) return;
-      
-      if (lang === '') {
-        callback(null, code);
-      }else if (!Prism.languages[lang]) {
-        
-        import(`https://cdnjs.cloudflare.com/ajax/libs/prism/1.19.0/components/prism-${lang}.min.js`)
-          .then(() => {
-            callback(null, Prism.highlight(code, Prism.languages[lang], lang));
-          }).catch(() => {
-            callback(null, code);
-          });
-      
-      } else {
-        callback(null, Prism.highlight(code, Prism.languages[lang], lang));
-      }
-    }
-  };
-
-  marked(content, options, (err, result) => {
-    content_parsed = result;
+  onMount(() => {
+    content_parsed = marked(content);
   });
 </script>
 
- <article class="post--single">
-
+<article class="post--single">
   <hgroup class="post__hgroup">
     <PostDate {created_at} format="m d, y" />
     <PostTitle title={title.replace('<br>', ' - ')} />
   </hgroup>
 
-  <div class="post__content">
+  <div class="post__content" use:prism>
     {@html content_parsed}
   </div>
 </article>
 
- <aside class="meta">
+<aside class="meta">
   <p class="meta__content">
     {#if github}
-      <GithubLink url="{github}" />,
+      <GithubLink url={github} />,
     {/if}
-    created by <a href="/about">Thomas Hurtig</a><br>
+    created by
+    <a href="/about">Thomas Hurtig</a><br />
   </p>
   <Tags {tags} />
 </aside>
