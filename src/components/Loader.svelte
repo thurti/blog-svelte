9ba6798a7f93e05e19a94e34ee7f23ae01e985aa
Thomas Hurtig <thurtig.email@gmail.com>
2020-11-28 19:03:46 +0100

    add delay option to Loader component

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -1,6 +1,30 @@
-<div id="loader">
-  <div class="inner1"></div>
-  <div class="inner2"></div>
-  <div class="inner3"></div><br>
-  <span class="small">loading</span>
-</div>
+<script>
+import { onMount, onDestroy } from "svelte";
+
+  export let delay = 500;
+
+  let show = false;
+  let loader_timeout;
+
+  onMount(() => {
+    //show loader if loading takes longer than 500ms
+    loader_timeout = window.setTimeout(() => {
+      show = true;
+    }, delay);
+  });
+
+  onDestroy(() => {
+    window.clearTimeout(loader_timeout);
+  });
+</script>
+
+{#if show}
+  <main>
+    <div id="loader">
+      <div class="inner1"></div>
+      <div class="inner2"></div>
+      <div class="inner3"></div><br>
+      <span class="small">loading</span>
+    </div>
+  </main>
+{/if}
