9a6a3fcb20322f07def0eeb3a3754aa3196cf386
Thomas Hurtig <thurtig.email@gmail.com>
2020-11-28 19:03:23 +0100

    add ErrorMessage component

---
+++ src/components/ErrorMessage.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let error;
+</script>
+
+<main>
+  <p class="text-center">
+    Something went wrong. <br>
+    {error.message} :/ 
+    <br><br>
+    <a href="/">Back Home</a>
+  </p>
+</main>
