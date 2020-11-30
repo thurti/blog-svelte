2c82a7d86c2c39ca77362ae237caab5d69834f46
Thomas Hurtig <thurtig.email@gmail.com>
2020-11-30 11:54:18 +0100

    made node compatible

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   url: 'https://t3000.uber.space',
-  api: location.hostname === 'localhost' ? 'http://localhost:3000/api/posts' : 'https://t3000.uber.space/api/posts',
+  api: (typeof location !== 'undefined' && location.hostname === 'localhost') ? 'http://localhost:3000/api/posts' : 'https://t3000.uber.space/api/posts',
   static: '/static',
   title: '| T3000 Blog',
   twitter: '@_thurti'
