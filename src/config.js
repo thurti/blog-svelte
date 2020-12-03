7e6a95cc6eff3dad5c0bca94723022b8dd1f4468
Thomas Hurtig <thurtig.email@gmail.com>
2020-12-03 18:08:02 +0100

    changed title

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   url: 'https://t3000.uber.space',
   api: (typeof location !== 'undefined' && location.hostname === 'localhost') ? 'http://localhost:3000/api/posts' : 'https://t3000.uber.space/api/posts',
-  title: '| T3000 Blog',
+  title: '| T3000',
   twitter: '@_thurti'
 }
