68a2c4c5208ea56f624f6823ab4e6aeababad9f6
Thomas Hurtig <thurtig.email@gmail.com>
2021-01-12 13:39:10 +0100

    created basiv nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let items;
+
+  //create store with current nav and check for is-active
+  //create views Article, Portfolio, PortfolioList
+  //check if show nav is set
+</script>
+
+<nav class="nav--main">
+  <ul>
+    {#each items as item}
+      <li class="is-active"><a href="{item.url}">{item.title}</a></li>
+    {/each}
+  </ul>
+</nav>
