ea57dbd18578313b61d4868835ff7e3b8d530833
Thomas Hurtig <thurtig.email@gmail.com>
2021-01-13 17:34:22 +0100

    add capitalize text

--- src/components/PostTitle.svelte
+++ src/components/PostTitle.svelte
@@ -4,19 +4,19 @@
 </script>
 
 {#if type === "h1"}
-  <h1 class="post__heading">
+  <h1 class="post__heading text-capitalize">
     {@html title}
   </h1>
 {/if}
 
 {#if type === "h2"}
-  <h2 class="post__heading">
+  <h2 class="post__heading text-capitalize">
     {@html title}
   </h2>
 {/if}
 
 {#if type === "h3"}
-  <h3 class="post__heading">
+  <h3 class="post__heading text-capitalize">
     {@html title}
   </h3>
 {/if}
--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -1,8 +1,10 @@
 <script>
   export let tags;
+
+  tags = tags.filter(tag => tag !== "home");
 </script>
 
-<p class="post-meta__tags">
+<p class="post-meta__tags text-capitalize">
   <b>Tags:</b>&nbsp;
   {#each tags as tag}<a href="/tag/{tag}">{tag}</a>,&nbsp;{/each}
 </p>
