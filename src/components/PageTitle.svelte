f542b7121662b2844e10d6903495f71b7ac62f22
Thomas <thurtig.email@gmail.com>
2021-02-12 12:48:28 +0100

    PageTitle add h1-3

--- src/components/PageTitle.svelte
+++ src/components/PageTitle.svelte
@@ -1,8 +1,15 @@
 <script>
   export let title;
+  export let h = 1;
   export let classname;
 </script>
 
 <div class={`grid page-title ${classname}`}>
-  <h1>{title}</h1>
+  {#if h == 1}
+    <h1>{title}</h1>
+  {:else if h == 2}
+    <h2>{title}</h2>
+  {:else if h == 3}
+    <h3>{title}</h3>
+  {/if}
 </div>
